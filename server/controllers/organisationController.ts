cc04186828e2ee69634e0384d370a1e53190727b
Mantas Imbrasas <mantasimbrasas@gmail.com>
2023-10-30 21:38:05 +0200

    additional commit for the routes

--- server/controllers/organisationController.ts
+++ server/controllers/organisationController.ts
@@ -1,2 +1,14 @@
 import { type Response } from "express";
 import { createOrganisation } from "../database/methods/organisation";
+
+export type createOrganisationRequest = {
+  body: {
+    name: string;
+    address: string;
+    city: string;
+    country: string;
+    email: string;
+    phone: string;
+    password: string;
+  };
+};
