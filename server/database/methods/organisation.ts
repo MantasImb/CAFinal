28cd905c74399a34df5f64fe10c5ec52e415c8c2
Mantas Imbrasas <mantasimbrasas@gmail.com>
2023-10-30 18:21:02 +0200

    add extra methods

--- server/database/methods/organisation.ts
+++ server/database/methods/organisation.ts
@@ -1,9 +1,13 @@
-import { Organisation } from "../models/Organisation"
+import { Organisation } from "../models/Organisation";
 
 export async function createOrganisation(organisationName: string) {
-  return Organisation.create({ organisationName })
+  return Organisation.create({ organisationName });
 }
 
 export async function getOrganisation(organisationId: string) {
-  return Organisation.findById(organisationId)
+  return Organisation.findById(organisationId);
+}
+
+export async function getOrganisationReservations(organisationId: string) {
+  return Organisation.findById(organisationId).populate("registrations");
 }
